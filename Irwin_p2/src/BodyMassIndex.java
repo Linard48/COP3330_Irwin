7c3b5ddff4e059c5b6be80b64b911eb0385d3907
Alex Irwin <alex.irwin@knights.ucf.edu>
2020-10-09 12:51:59 -0400

    correct files for p2

--- Irwin_p2/src/BodyMassIndex.java
+++ Irwin_p2/src/BodyMassIndex.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BodyMassIndex {
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    Scanner scanner = new Scanner(System.in);
+    private double weight, height;
+
+    public BodyMassIndex(double height, double weight) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+
+    public double calculateBmi() {
+        return (weight * 703) / (height * height);
+    }
+
+    public String calculateCategory() {
+        double bmi_val = calculateBmi();
+        String category;
+        if (bmi_val <= 18.5) {
+            category = "Underweight";
+        } else if (bmi_val < 25) {
+            category = "Normal weight";
+        } else if (bmi_val < 30) {
+            category = "Overweight";
+        } else {
+            category = "Obese";
+        }
+        return category;
+    }
 }
--- Irwin_p2/src/BodyMassIndexTest.java
+++ Irwin_p2/src/BodyMassIndexTest.java
@@ -1,4 +1,46 @@
 import static org.junit.jupiter.api.Assertions.*;
-class BodyMassIndexTest {
-  
+import org.testng.annotations.Test;
+
+public class BodyMassIndexTest {
+    //
+    // Considering what is in my BodyMassIndex Class, Doesn't the follow category tests
+    // also show that the methods in said Class work properly?
+    //
+    @Test
+    public void testBmiCalcUnderweight(){
+        BodyMassIndex bmi = new BodyMassIndex(68,100);
+
+        double answer = bmi.calculateBmi();
+
+        assertEquals(15.20, answer, 0.01);
+    }
+
+    @Test
+    public void testBmiCalcOverweight(){
+        BodyMassIndex bmi = new BodyMassIndex(50,100);
+
+        double answer = bmi.calculateBmi();
+
+        assertEquals(28.12, answer, 0.01);
+    }
+
+    @Test
+    public void testBmiCalcNormalWeight(){
+        BodyMassIndex bmi = new BodyMassIndex(55,100);
+
+        double answer = bmi.calculateBmi();
+
+        assertEquals(23.24, answer, 0.01);
+    }
+
+    @Test
+    public void testBmiCalcObese(){
+        BodyMassIndex bmi = new BodyMassIndex(40,100);
+
+        double answer = bmi.calculateBmi();
+
+        assertEquals(43.94, answer, 0.01);
+    }
+
+
 }
